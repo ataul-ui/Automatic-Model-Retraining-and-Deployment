dbbb7f2301517b106b5376ac3343f83c3b848711
Ataulhaq Akbar <ataul.akbar@gmail.com>
2023-07-05 17:40:57 -0400

    added code to main.py

--- main.py
+++ main.py
@@ -76,3 +76,55 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     go(args)
+    
+    
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import mlflow
+import mlflow.sklearn
+import wandb
+
+# Load the dataset
+data = pd.read_csv("machine_failure_dataset.csv")
+
+# Split the dataset into features and target
+X = data.drop("failure", axis=1)
+y = data["failure"]
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a random forest classifier
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Start MLflow experiment
+mlflow.set_experiment("Predictive Maintenance")
+
+with mlflow.start_run() as run:
+    # Log parameters and dataset information
+    mlflow.log_params({"n_estimators": model.n_estimators})
+    mlflow.log_param("test_size", 0.2)
+    mlflow.log_dataset_info(X=X_train, y=y_train, data_format="csv")
+    
+    # Train the model
+    model.fit(X_train, y_train)
+    
+    # Evaluate the model
+    accuracy = model.score(X_test, y_test)
+    mlflow.log_metric("accuracy", accuracy)
+    
+    # Log the model artifact
+    mlflow.sklearn.log_model(model, "model")
+    
+    # Log experiment to Wandb
+    wandb.log({"accuracy": accuracy})
+
+    # Save the experiment run ID for future reference
+    run_id = run.info.run_id
+
+# Log MLflow and Wandb experiment links
+mlflow_link = f"https://mlflow.server.com/experiments/{run.info.experiment_id}/runs/{run_id}"
+wandb_link = wandb.run.get_url()
+print(f"MLflow experiment link: {mlflow_link}")
+print(f"Wandb experiment link: {wandb_link}")
