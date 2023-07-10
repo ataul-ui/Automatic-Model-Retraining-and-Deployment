664188404228cadcf4e476526c7d6e7618c4328b
Ataulhaq Akbar <ataul.akbar@gmail.com>
2023-07-10 14:32:54 -0400

    imported requests library to app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
 from ml_code import test_predictive_maintaincence
+import requests
+
+'''
+
+response = requests.post('/url/to/query/')
+# Assert that the response is successful (status code 200)
+assert response.status_code == 200
+'''
 
 app = FastAPI()
 
