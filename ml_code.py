bb10c552e3ab7f10ca4c26a6bea2cfaed49b1807
Ataulhaq Akbar <ataul.akbar@gmail.com>
2023-07-11 00:07:57 -0400

    atleast I am getting a result. the code I wrote in testing.py will eventually be moved to ml_code.py

--- ml_code.py
+++ ml_code.py
@@ -6,6 +6,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 import mlflow
 import mlflow.sklearn
+import json
 
 
 
@@ -26,15 +27,16 @@ class Model:
                 
         # Split the dataset into features and target
         X = self.data.drop("Machine failure", axis=1)
+        #I'll also need to drop UDI
         y = self.data["Machine failure"]
 
         # Split the data into training and testing sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        mlflow.start_run()
+        #mlflow.start_run()
 
         # Create a logistic regression classifier
-        model = LogisticRegression(random_state=42)
+        model = LogisticRegression(random_state=42, max_iter=1000, solver='saga')
 
         # Train the model
         model.fit(X_train, y_train)
@@ -46,7 +48,7 @@ class Model:
         accuracy = accuracy_score(y_test, y_pred)
         #print("Accuracy:", accuracy)
         
-        return accuracy
+        return model
 
 
 def give_back_the_response(type, air_temp, proc_temp, rot_temp, torque, tool_wear):
@@ -69,9 +71,78 @@ def test_predictive_maintaincence():
     thing = Model(data)
     result = thing.trained_model()
     
+    
     print("Accuracy:", result)
     
     return result
+
+def result_return():
+    
+    data = pd.read_csv("machine_failure.csv")
+    
+    thing = Model(data)
+    result = thing.trained_model()
+    
+    first_json_data = '''
+    {
+        "UDI": 50,
+        "Product ID": "L47438",
+        "Type": "M",
+        "Air temperature [K]": 298.1,
+        "Process temperature [K]": 309.1,
+        "Rotational speed [rpm]": 1527,
+        "Torque [Nm]": 28.6,
+        "Tool wear [min]": 9,
+        "TWF": "0",
+        "HDF": "2",
+        "PWF": "0",
+        "OSF": "0",
+        "RNF": "0"
+    }
+    '''
+    
+    json_data = '''
+{
+    "UDI": 50,
+    "Product ID": "L47438",
+    "Type": "M",
+    "Air temperature [K]": 298.1,
+    "Process temperature [K]": 309.1,
+    "Rotational speed [rpm]": 1527,
+    "Torque [Nm]": 28.6,
+    "Tool wear [min]": 9,
+    "TWF": "0",
+    "HDF": "2",
+    "PWF": "0",
+    "OSF": "0",
+    "RNF": "0",
+    "OSF": "0"
+}
+'''
+
+
+    # Parse the JSON data
+    data = json.loads(json_data)
+    # Convert the data to a DataFrame
+    df = pd.DataFrame([data], index=[0])
+
+    # Print the DataFrame
+    #print(df)
+    
+    # Assuming you have a new data point stored in a DataFrame called 'new_data'
+    label_encoder = LabelEncoder()
+    for column in df.columns:
+        if df[column].dtype == "object":
+            df[column] = label_encoder.fit_transform(df[column])
+
+    
+    predictions = result.predict(df)
+    
+    print(predictions)
+
+    
+    
+    return "off"
     '''
     # Convert non-numeric columns to numeric using label encoding
     label_encoder = LabelEncoder()
@@ -116,4 +187,4 @@ def test_predictive_maintaincence():
     
     
 if __name__ == "__main__":
-    test_predictive_maintaincence()
+    result_return()
