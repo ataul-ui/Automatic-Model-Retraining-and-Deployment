883bd8e8083e2c216d73cb8c5c1cc4df15027a06
Ataulhaq Akbar <77755531+ataul-ui@users.noreply.github.com>
2023-09-28 14:22:00 -0400

    Update ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -12,9 +12,6 @@ import mlflow
 import mlflow.sklearn
 import json
 
-#LOOK AT INTRO TO AI PROJECT I DID
-#TWO YEARS AGO ON HOW TO WRITE ML MODELS
-#USING CLASSES
 class Model:
     
     instance_created = False
@@ -36,15 +33,6 @@ class Model:
     def training(self):
         
         self.pre_processing()
-        '''
-        space = {
-        #'C': hp.loguniform('C', low=-3, high=3),
-        'C': hp.uniform('C', 0.01, 10),
-        'penalty': hp.choice('penalty', ['l1', 'l2', 'elasticnet', 'none']),
-        'l1_ratio': hp.uniform('l1_ratio', 0, 1),
-        #'solver': hp.choice('solver', ['newton-cg', 'lbfgs', 'sag', 'saga']),
-        'fit_intercept': hp.choice('fit_intercept', [True, False])
-        }'''
         space = {
             'warm_start': hp.choice('warm_start', [True, False]),
             'fit_intercept': hp.choice('fit_intercept', [True, False]),
@@ -95,62 +83,9 @@ class Model:
 
         return best_model
         
-        '''model = LogisticRegression(random_state=42, max_iter=10000, solver='saga')
-        # Start MLflow experiment
-        mlflow.set_experiment("Predictive Maintenance")
-        model.fit(self.X_train, self.y_train)
-        
-        # Evaluate the model
-        accuracy = model.score(self.X_test, self.y_test)
-        second_accuracy = model.score(self.X_train, self.y_train)
-        mlflow.log_metric("accuracy", accuracy)
-        mlflow.log_metric("second_accuracy", second_accuracy)
-    
-        # Log the model artifact
-        mlflow.sklearn.log_model(model, "model")
-        return model'''
 
 
     
-    def logistic_model_result(self):
-        if self.__class__.trained_or_not:
-            return "return the model result"
-        else:
-            self.__class__.trained_or_not = True
-            return "train the model, then return the result"
-    '''
-    def logistic_regression_model(self):
-        label_encoder = LabelEncoder()
-        for column in self.data.columns:
-            if self.data[column].dtype == "object":
-                self.data[column] = label_encoder.fit_transform(self.data[column])
-        #use this as reference for feature engineering
-        # and xgboost building:
-        # https://www.kaggle.com/code/yantxx/xgboost-binary-classifier-machine-failure
-        #do preprocessig on the data, if machine type 
-        # is not M or L, then make it other
-
-        X = self.data.drop(["Machine failure", "UDI", "Product ID", "TWF", "HDF", "PWF", "OSF", "RNF"], axis=1)
-        y = self.data["Machine failure"]
-
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-        model = LogisticRegression(random_state=42, max_iter=10000, solver='saga')
-        # Start MLflow experiment
-        mlflow.set_experiment("Predictive Maintenance")
-        model.fit(X_train, y_train)
-        
-        # Evaluate the model
-        accuracy = model.score(X_test, y_test)
-        second_accuracy = model.score(X_train, y_train)
-        mlflow.log_metric("accuracy", accuracy)
-        mlflow.log_metric("second_accuracy", second_accuracy)
-    
-        # Log the model artifact
-        mlflow.sklearn.log_model(model, "model")
-
-        return model
-    '''
     
 
 
@@ -177,19 +112,7 @@ def result_return(data_input):
     # Convert the data to a DataFrame
     df = pd.DataFrame(data)
 
-    # Assuming you have a new data point stored in a DataFrame called 'new_data'
     
-    '''label_encoder = LabelEncoder()
-    for column in df.columns:
-        if df[column].dtype == "object":
-            df[column] = label_encoder.fit_transform(df[column])'''
-    """label_encoder = LabelEncoder()
-    for column in df.columns:
-        if isinstance(df[column][0], list):
-            # Flatten the list and convert it to a single value
-            df[column] = [item[0] for item in df[column]]
-        if df[column].dtype == "object":
-            df[column] = label_encoder.fit_transform(df[column])"""
     label_encoder = LabelEncoder()
     for column in df.columns:
         if isinstance(df[column][0], list):
@@ -204,14 +127,6 @@ def result_return(data_input):
                 df[column] = label_encoder.fit_transform(df[column].astype(str))
 
     predictions = result.predict(df)
-    '''want_test = pd.DataFrame([0,1])
-    if predictions[0] == want_test[0].all():
-        print("success")
-    elif predictions[0] == want_test[1].all():
-        print("whatever")
-    else:
-        print("what happened?")
-    print(predictions[0])'''
     
     
     
@@ -238,12 +153,7 @@ if __name__ == "__main__":
     json_data = json.dumps(json_code)
     the_answer = result_return(json_data)
     want_test = pd.DataFrame([0,1])
-    '''
-    print(want_test[0][1])
-    print("anoth")
-    return "nothig"
-    
-    '''
+
     if the_answer[0] == want_test[0][0]:
         print("machine will fail")
     elif the_answer[0] == want_test[0][1]:
